ef85481addafd04f4488784bf3478327c153feab
Heng-Chang Chen <chen.hc.inaki@gmail.com>
2022-05-02 12:45:54 +0200

    Update IS_cART_Einkauf_et_al_2022.MSigDB_C7.R

--- IS_cART_Einkauf_et_al_2022.MSigDB_C7.R
+++ IS_cART_Einkauf_et_al_2022.MSigDB_C7.R
@@ -34,27 +34,35 @@ msigdb_cART_EC_tous_ID_unique <- msigdb_cART_EC_tous %>% dplyr::select(ID) %>% u
 
 msigdb_cART_EC_tous.cART_untreat_avoir <- dplyr::inner_join(msigdb_cART_EC_tous.cART_untreat, msigdb_cART_EC_tous_ID_unique, by = "ID") %>% dplyr::select(ID, Ratio)
 msigdb_cART_EC_tous.cART_untreat_sans <- dplyr::anti_join(msigdb_cART_EC_tous_ID_unique, msigdb_cART_EC_tous.cART_untreat, by = "ID") %>% dplyr::select(ID) %>% dplyr::mutate(Ratio = 0)
-msigdb_cART_EC_tous.cART_untreat.ID.Ratio <- dplyr::bind_rows(msigdb_cART_EC_tous.cART_untreat_avoir, msigdb_cART_EC_tous.cART_untreat_sans) %>% dplyr::rename(cART_untreat = Ratio)
+msigdb_cART_EC_tous.cART_untreat.ID.Ratio <- dplyr::bind_rows(msigdb_cART_EC_tous.cART_untreat_avoir, msigdb_cART_EC_tous.cART_untreat_sans) %>% dplyr::rename(cART_untreat = Ratio) %>% arrange(desc(ID))
+rownames(msigdb_cART_EC_tous.cART_untreat.ID.Ratio) <- NULL
 
 msigdb_cART_EC_tous.cART_long_avoir <- dplyr::inner_join(msigdb_cART_EC_tous.cART_long, msigdb_cART_EC_tous_ID_unique, by = "ID") %>% dplyr::select(ID, Ratio)
 msigdb_cART_EC_tous.cART_long_sans <- dplyr::anti_join(msigdb_cART_EC_tous_ID_unique, msigdb_cART_EC_tous.cART_long, by = "ID") %>% dplyr::select(ID) %>% dplyr::mutate(Ratio = 0)
-msigdb_cART_EC_tous.cART_long.ID.Ratio <- dplyr::bind_rows(msigdb_cART_EC_tous.cART_long_avoir, msigdb_cART_EC_tous.cART_long_sans) %>% dplyr::rename(cART_short = Ratio)
+msigdb_cART_EC_tous.cART_long.ID.Ratio <- dplyr::bind_rows(msigdb_cART_EC_tous.cART_long_avoir, msigdb_cART_EC_tous.cART_long_sans) %>% dplyr::rename(cART_long = Ratio) %>% arrange(desc(ID))
+rownames(msigdb_cART_EC_tous.cART_long.ID.Ratio) <- NULL
 
 msigdb_cART_EC_tous.cART_short_avoir <- dplyr::inner_join(msigdb_cART_EC_tous.cART_short, msigdb_cART_EC_tous_ID_unique, by = "ID") %>% dplyr::select(ID, Ratio)
 msigdb_cART_EC_tous.cART_short_sans <- dplyr::anti_join(msigdb_cART_EC_tous_ID_unique, msigdb_cART_EC_tous.cART_short, by = "ID") %>% dplyr::select(ID) %>% dplyr::mutate(Ratio = 0)
-msigdb_cART_EC_tous.cART_short.ID.Ratio <- dplyr::bind_rows(msigdb_cART_EC_tous.cART_short_avoir, msigdb_cART_EC_tous.cART_short_sans) %>% dplyr::rename(cART_long = Ratio)
+msigdb_cART_EC_tous.cART_short.ID.Ratio <- dplyr::bind_rows(msigdb_cART_EC_tous.cART_short_avoir, msigdb_cART_EC_tous.cART_short_sans) %>% dplyr::rename(cART_short = Ratio) %>% arrange(desc(ID))
+rownames(msigdb_cART_EC_tous.cART_short.ID.Ratio) <- NULL
 
 msigdb_cART_EC_tous.EC_long_avoir <- dplyr::inner_join(msigdb_cART_EC_tous.EC_long, msigdb_cART_EC_tous_ID_unique, by = "ID") %>% dplyr::select(ID, Ratio)
 msigdb_cART_EC_tous.EC_long_sans <- dplyr::anti_join(msigdb_cART_EC_tous_ID_unique, msigdb_cART_EC_tous.EC_long, by = "ID") %>% dplyr::select(ID) %>% dplyr::mutate(Ratio = 0)
-msigdb_cART_EC_tous.EC_long.ID.Ratio <- dplyr::bind_rows(msigdb_cART_EC_tous.EC_long_avoir, msigdb_cART_EC_tous.EC_long_sans) %>% dplyr::rename(EC_long = Ratio)
+msigdb_cART_EC_tous.EC_long.ID.Ratio <- dplyr::bind_rows(msigdb_cART_EC_tous.EC_long_avoir, msigdb_cART_EC_tous.EC_long_sans) %>% dplyr::rename(EC_long = Ratio) %>% arrange(desc(ID))
+rownames(msigdb_cART_EC_tous.EC_long.ID.Ratio) <- NULL
 
-msigdb_cART_EC_tous.ID.Ratio <- dplyr::bind_cols(msigdb_cART_EC_tous.cART_untreat.ID.Ratio, msigdb_cART_EC_tous.cART_long.ID.Ratio, msigdb_cART_EC_tous.cART_short.ID.Ratio, msigdb_cART_EC_tous.EC_long.ID.Ratio) %>% dplyr::select(ID...1, cART_untreat, cART_short, cART_long, EC_long)
+msigdb_cART_EC_tous.ID.Ratio <- dplyr::bind_cols(msigdb_cART_EC_tous.cART_untreat.ID.Ratio, msigdb_cART_EC_tous.cART_short.ID.Ratio, msigdb_cART_EC_tous.cART_long.ID.Ratio, msigdb_cART_EC_tous.EC_long.ID.Ratio) %>% dplyr::select(ID...1, cART_untreat, cART_short, cART_long, EC_long)
 row.names(msigdb_cART_EC_tous.ID.Ratio) <- msigdb_cART_EC_tous.ID.Ratio$ID...1
 msigdb_cART_EC_tous.ID.Ratio$ID...1 <- NULL
 msigdb_cART_EC_tous.ID.Ratio.mx <- data.matrix(msigdb_cART_EC_tous.ID.Ratio, rownames.force = T)
 
-pdf("/media/chen/DATA/evoPath/Abb/msigdb_cART_EC_evolution_pathway.pdf")  
-Heatmap(msigdb_cART_EC_tous.ID.Ratio.mx, name = "GeneRatio/BgRatio", rect_gp = gpar(col = "white", lwd = 0.5), column_title = "clinical condition", row_title = "C7: immunologic signatures", column_title_side = "bottom", row_dend_width = unit(4, "cm"), row_names_gp = gpar(fontsize = 10), column_names_gp = gpar(fontsize = 10), row_km = 5)
+pdf("/media/chen/DATA/evoPath/Abb/msigdb_cART_EC_evolution_pathway.pdf", height = 5, width = 4)  
+Heatmap(msigdb_cART_EC_tous.ID.Ratio.mx, name = "GeneRatio/BgRatio", rect_gp = gpar(col = "black", lwd = 0.01), column_title = "clinical condition", row_title = "C7: immunologic signatures", column_title_side = "bottom", row_dend_width = unit(4, "cm"), show_row_names = F, column_names_gp = gpar(fontsize = 10))
+dev.off()
+
+svg("/media/chen/DATA/evoPath/Abb/msigdb_cART_EC_evolution_pathway.svg", height = 5, width = 4)  
+Heatmap(msigdb_cART_EC_tous.ID.Ratio.mx, name = "GeneRatio/BgRatio", rect_gp = gpar(col = "black", lwd = 0.01), column_title = "clinical condition", row_title = "C7: immunologic signatures", column_title_side = "bottom", row_dend_width = unit(4, "cm"), show_row_names = F, column_names_gp = gpar(fontsize = 10))
 dev.off()
 
 # 2. Pathway overlapped
